791471f3f2a4265facdd1c65818220b8c6c827d1
barenas21 <b.arenas21@gmail.com>
2024-01-26 14:55:03 -0800

    Add basic Python slackbot app

--- adhoc_admin/app.py
+++ adhoc_admin/app.py
@@ -1,3 +1,19 @@
 import os
 
 from slack_bolt import App
+
+# Initializes your app with your bot token and signing secret
+app = App(
+    token=os.environ.get("SLACK_BOT_TOKEN"),
+    signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
+)
+
+# Listens to incoming slash commands "/adhoc-admin request"
+@app.command("/adhoc-admin")
+def handle_adhoc_admin(ack, body, logger):
+    ack()
+    logger.info(body)
+
+# Start your app
+if __name__ == "__main__":
+    app.start(port=int(os.environ.get("PORT", 3000)))
